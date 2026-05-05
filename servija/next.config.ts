46467c153ed51e717c6607b411af3a1e7f26fcac
GabrielRosa1 <gabrielrosamart@gmail.com>
2026-05-04 22:16:51 -0300

    refactor: remove API rewrites configuration from next.config.ts

--- servija/next.config.ts
+++ servija/next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://44.214.178.65:8080/:path*',
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
